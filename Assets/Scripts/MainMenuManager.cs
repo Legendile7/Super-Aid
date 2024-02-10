07569a6abe73b2c9110854334bdc7bd0318d25c2
Legendile7 <diveshg9@gmail.com>
2024-02-10 12:11:49 -0500

    Push notitifcations and google maps directrions

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 using Firebase.Database;
 using System.Collections;
 using UnityEngine.Networking;
+using Google.MiniJSON;
+using Unity.Notifications.Android;
+using UnityEngine.Android;
+using Unity.Notifications;
+using System;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -20,6 +26,51 @@ public class MainMenuManager : MonoBehaviour
 
     int counter = 0;
     string apiKey = "AIzaSyA_T8ZTrCAaPe7zVrrvozwyt9tGsPs7fug";
+    
+    // Create a class to represent the structure of your JSON data
+    [Serializable]
+    public class NotificationData
+    {
+        public string origin;
+        public string destination;
+    }
+
+    [System.Serializable]
+    public class GoogleMapsResponse
+    {
+        public string[] destination_addresses;
+        public string[] origin_addresses;
+        public Row[] rows;
+        public string status;
+    }
+
+    [System.Serializable]
+    public class Row
+    {
+        public Element[] elements;
+    }
+
+    [System.Serializable]
+    public class Element
+    {
+        public Distance distance;
+        public Duration duration;
+        public string status;
+    }
+
+    [System.Serializable]
+    public class Distance
+    {
+        public string text;
+        public int value;
+    }
+
+    [System.Serializable]
+    public class Duration
+    {
+        public string text;
+        public int value;
+    }
 
     // Method to send the request to the Google Distance Matrix API
     public IEnumerator SendRequest(string origins, string destinations)
@@ -44,14 +95,99 @@ public class MainMenuManager : MonoBehaviour
                 // Parse the response data
                 string responseText = webRequest.downloadHandler.text;
                 Debug.Log("Response received: " + responseText);
+                GoogleMapsResponse response = JsonUtility.FromJson<GoogleMapsResponse>(responseText);
+
+                if (response != null && response.rows.Length > 0 && response.rows[0].elements.Length > 0)
+                {
+                    int durationValue = response.rows[0].elements[0].duration.value;
+                    Debug.Log("Duration Value: " + durationValue);
+                    if (durationValue < 600)
+                    {
+                        Debug.Log("Duration is less than 10 minutes, sending notification");
+                        float distanceValue = response.rows[0].elements[0].distance.value;
+                        float miles = distanceValue / 1609.344f;
+                        SendNotification(miles, origins, destinations);
+                    }
+                    else
+                    {
+                        Debug.Log("Duration is more than 10 minutes.");
+                        float distanceValue = response.rows[0].elements[0].distance.value;
+                        float miles = distanceValue / 1609.344f;
+                        SendNotification(miles, origins, destinations);
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Failed to parse JSON response or response is empty.");
+                }
             }
         }
     }
+    void SendNotification(float distanceValue, string origin, string destination)
+    {
+        Debug.Log("Sending notification");
+        var notification = new AndroidNotification();
+        notification.Title = "NEW ALERT!";
+        notification.Text = "A new help request is available in your area. Distance: " + distanceValue + " miles";
+        notification.FireTime = System.DateTime.Now.AddSeconds(1);
+        notification.IntentData = "&origin=" + origin + "&destination=" + destination;
+        var id = AndroidNotificationCenter.SendNotification(notification, "default");
+    }
     private void Start()
     {
+        var notificationIntentData = AndroidNotificationCenter.GetLastNotificationIntent();
+        if (notificationIntentData != null)
+        {
+            Debug.Log("Data Found!");
+            var notification = notificationIntentData.Notification;
+            string url = "https://www.google.com/maps/dir/?api=1" + notification.IntentData;
+            Application.OpenURL(url);
+        }
         Input.location.Start(10f, 0.1f);
         databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
         databaseReference.OrderByChild("requests").ChildAdded += HandleChildAdded;
+        var args = NotificationCenterArgs.Default;
+        args.AndroidChannelId = "default";
+        args.AndroidChannelName = "Notifications";
+        args.AndroidChannelDescription = "Main notifications";
+        NotificationCenter.Initialize(args);
+        StartCoroutine(Init());
+    }
+    private void OnApplicationPause(bool pause)
+    {
+        if (!pause)
+        {
+            var notificationIntentData = AndroidNotificationCenter.GetLastNotificationIntent();
+            if (notificationIntentData != null)
+            {
+                notificationIntentData = null;
+                Debug.Log("Data Found!");
+                var notification = notificationIntentData.Notification;
+                string url = "https://www.google.com/maps/dir/?api=1" + notification.IntentData;
+                Application.OpenURL(url);
+            }
+        }
+    }
+    IEnumerator Init()
+    {
+        var request = NotificationCenter.RequestPermission();
+        if (request.Status == NotificationsPermissionStatus.RequestPending)
+            yield return request;
+        Debug.Log("Permission result: " + request.Status);
+    }
+    internal void PermissionCallbacks_PermissionDeniedAndDontAskAgain(string permissionName)
+    {
+        Debug.Log($"{permissionName} PermissionDeniedAndDontAskAgain");
+    }
+
+    internal void PermissionCallbacks_PermissionGranted(string permissionName)
+    {
+        Debug.Log($"{permissionName} PermissionCallbacks_PermissionGranted");
+    }
+
+    internal void PermissionCallbacks_PermissionDenied(string permissionName)
+    {
+        Debug.Log($"{permissionName} PermissionCallbacks_PermissionDenied");
     }
     void HandleChildAdded(object sender, ChildChangedEventArgs args)
     {
@@ -144,5 +280,10 @@ public class MainMenuManager : MonoBehaviour
             //Write to database
             writeNewUser();
         }
+    }
+
+    public void LoadScene(int id)
+    {
+        SceneManager.LoadScene(id);
     }
 }
